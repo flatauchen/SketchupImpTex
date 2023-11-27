d5c952eb150a71e9b4484a7460220122427abf4b
Flavio Tauchen <flatauchen@gmail.com>
2023-11-27 14:25:10 +0000

    Adicionado Commit_Operation no processo

--- ImpTex/main.rb
+++ ImpTex/main.rb
@@ -58,11 +58,9 @@ end
 # Módulo do desenvolvedor
 module FlaTauchen
 
-	# Módulo de Importação
-	module TiImpTex
+	module TiImpTex # Módulo de Importação
 
-		# Classe Diálogo
-		class DialogoDimensoes
+		class DialogoDimensoes # Classe Diálogo
 			def initialize
 				@img_larg = "0"
 				@img_alt = "0"
@@ -82,7 +80,7 @@ module FlaTauchen
 					lang_altura		= "Height:"
 					lang_dimencent	= "Dimensions in Centimeters"
 					lang_dimeinv	= "The dimensions are not valid!"
-				end
+				end # fim do if
 
 				loop do
 					prompts = [lang_largura, lang_altura]
@@ -99,17 +97,16 @@ module FlaTauchen
 						@img_larg = input[0].gsub(',', '.').to_f
 						@img_alt = input[1].gsub(',', '.').to_f
 						break
-					end
-				end
-			end
+					end # fim do if
+				end # fim do loop
+			end # fim do método criar_dialogo
 
 			def obter_dimensoes
 				[@img_larg, @img_alt]
-			end
+			end # fim do método obter_dimensoes
 		end
 
-		# Classe Pasta
-		class ImpPas
+		class ImpPas # Classe Pasta
 
 			def initialize
 				@texturas_pasta = escolher_pasta
@@ -117,7 +114,7 @@ module FlaTauchen
 					@img_larg, @img_alt = obter_dimensoes_usuario
 					importar_pasta(@img_larg, @img_alt)
 				end
-			end
+			end # fim do método initialize
 
 			def importar_pasta(largura_padrao, altura_padrao)
 
@@ -136,6 +133,10 @@ module FlaTauchen
 					lang_formato	= "Files named with measurements\neg: 'Description Width x Height .jpg\/png' can be imported automatically."
 				end
 
+				model = Sketchup.active_model
+				operation = model.start_operation('Importar Texturas', true) # Inicia a operação
+				texturas_para_importar = []
+
 				@texturas_pasta.each do |textura|
 					# Obtém o nome do material a partir do nome do arquivo
 					nome_material = File.basename(textura, File.extname(textura))
@@ -146,7 +147,6 @@ module FlaTauchen
 
 					if tamanho
 					tamanho = tamanho.map(&:to_f)
-					puts "Tamanho: " + tamanho.to_s
 							img_larg = tamanho.max
 							img_alt = tamanho.min
 					else
@@ -154,7 +154,6 @@ module FlaTauchen
 							UI.messagebox("#{lang_semdim12}#{nome_material}#{lang_semdim22}\n#{lang_formato}", MB_OK)
 							loop do
 								img_larg, img_alt = obter_dimensoes_usuario
-								puts "Tamanho: \[" + img_larg.to_s + ", " + img_alt.to_s + "\]"
 								para_todos = UI.messagebox(lang_aplictd, MB_YESNO)
 
 								if para_todos == IDNO
@@ -173,25 +172,32 @@ module FlaTauchen
 						else
 							img_larg = largura_padrao
 							img_alt = altura_padrao
-							puts "Tamanho: \[" + img_larg.to_s + ", " + img_alt.to_s + "\]"
 						end
 					end
 
-					# Adiciona um novo material ao modelo ativo
-					material = Sketchup.active_model.materials.add(nome_material)
-
-					# Define a textura do material usando o arquivo da textura
-					material.texture = textura
-
-					# Ajusta as dimensões da textura conforme a orientação da imagem
-					puts "Material: " + nome_material
-					puts "Dimensões: Largura " + img_larg.to_s + " x Altura " + img_alt.to_s
-					puts "Imagem: " + textura
-					puts ""
-					material.texture.size = (img_larg > img_alt) ? [img_larg.cm, img_alt.cm] : [img_alt.cm, img_larg.cm]
+					texturas_para_importar << {
+						nome_material: nome_material,
+						textura: textura,
+						img_larg: img_larg,
+						img_alt: img_alt
+					}
 				end
 
-			end
+				# Executa todas as operações fora do loop
+				texturas_para_importar.each do |textura_info|
+					material = model.materials.add(textura_info[:nome_material])
+					material.texture = textura_info[:textura]
+					material.texture.size = (textura_info[:img_larg] > textura_info[:img_alt]) ? [textura_info[:img_larg].cm, textura_info[:img_alt].cm] : [textura_info[:img_alt].cm, textura_info[:img_larg].cm]
+					largura = textura_info[:img_larg]
+					altura = textura_info[:img_alt]
+					puts "Material: " + textura_info[:nome_material]
+					puts "Dimensões: Largura #{largura} x Altura #{altura}"
+					puts "Imagem: " + textura_info[:textura]
+					puts ""
+				end # fim do loop
+				model.commit_operation # Finaliza a operação
+
+			end # fim do método importar_pasta
 
 			def escolher_pasta
 
@@ -214,22 +220,21 @@ module FlaTauchen
 						return arq_img
 					end
 				end
-			end
+			end # fim do método escolher_pasta
 
 			def obter_dimensoes_usuario
 				dialogo = TiImpTex::DialogoDimensoes.new
 				dialogo.obter_dimensoes
-			end
+			end # fim do método obter_dimensoes_usuario
 
 			def validar_dimensoes(largura, altura)
 				return false if largura.to_f.zero? || altura.to_f.zero?
 				true
-			end
+			end # fim do método validar_dimensoes
 
-		end
+		end # fim da classe ImpPas
 
-		# Classe Imagem
-		class ImpImg
+		class ImpImg # Classe Imagem
 
 			def initialize
 				@textura_arquivo = escolher_arquivo
@@ -254,39 +259,41 @@ module FlaTauchen
 					lang_formato	= "Files named with measurements\neg: 'Description Width x Height .jpg\/png' can be imported automatically."
 				end
 
+				model = Sketchup.active_model
+				operation = model.start_operation('Importar Texturas', true) # Inicia a operação
+
+				# Define nome do material e dimensões
 				nome_material = File.basename(@textura_arquivo, File.extname(@textura_arquivo))
 				nome_tamanho = nome_material.gsub(',', '.')
 				tamanho = nome_tamanho.downcase.scan(/(\d+(?:\.\d+)?)x(\d+(?:\.\d+)?)/).last
 
-					if largura_padrao.to_f.zero? && altura_padrao.to_f.zero?
-						if tamanho
-							tamanho = tamanho.map(&:to_f)
-							puts "Tamanho: " + tamanho.to_s
-							img_larg = tamanho.max
-							img_alt = tamanho.min
-						else
-							UI.messagebox("#{lang_semdim12}#{nome_material}#{lang_semdim22}\n#{lang_formato}", MB_OK)
-							loop do
-								img_larg, img_alt = obter_dimensoes_usuario
-								puts "Tamanho: \[" + img_larg.to_s + ", " + img_alt.to_s + "\]"
-								break if validar_dimensoes(img_larg, img_alt)
-								UI.messagebox(lang_dimeinv, MB_OK)
-							end
-						end
+				if largura_padrao.to_f.zero? && altura_padrao.to_f.zero?
+					if tamanho
+						tamanho = tamanho.map(&:to_f)
+						img_larg = tamanho.max
+						img_alt = tamanho.min
 					else
-						img_larg = largura_padrao
-						img_alt = altura_padrao
-						puts "Tamanho: \[" + img_larg.to_s + ", " + img_alt.to_s + "\]"
+						UI.messagebox("#{lang_semdim12}#{nome_material}#{lang_semdim22}\n#{lang_formato}", MB_OK)
+						loop do
+							img_larg, img_alt = obter_dimensoes_usuario
+							break if validar_dimensoes(img_larg, img_alt)
+							UI.messagebox(lang_dimeinv, MB_OK)
+						end # fim do loop
 					end
+				else
+					img_larg = largura_padrao
+					img_alt = altura_padrao
+				end # fim do if
 
 				material = Sketchup.active_model.materials.add(nome_material)
 				material.texture = @textura_arquivo
-					puts "Material: " + nome_material
-					puts "Dimensões: Largura " + img_larg.to_s + " x Altura " + img_alt.to_s
-					puts "Imagem: " + @textura_arquivo
-					puts ""
 				material.texture.size = (img_larg > img_alt) ? [img_larg.cm, img_alt.cm] : [img_alt.cm, img_larg.cm]
-			end
+				puts "Material: " + nome_material
+				puts "Dimensões: Largura " + img_larg.to_s + " x Altura " + img_alt.to_s
+				puts "Imagem: " + @textura_arquivo
+				puts ""
+				model.commit_operation # Finaliza a operação
+			end # fim do método importar_arquivo
 
 			def escolher_arquivo
 
@@ -302,18 +309,18 @@ module FlaTauchen
 				if arquivo && (arquivo.end_with?('.jpg', '.png'))
 					return arquivo.gsub('\\', '/')
 				end
-			end
+			end # fim do método escolher_arquivo
 
 			def obter_dimensoes_usuario
 				dialogo = TiImpTex::DialogoDimensoes.new
 				dialogo.obter_dimensoes
-			end
+			end # fim do método obter_dimensoes_usuario
 
 			def validar_dimensoes(largura, altura)
 				return false if largura.to_f.zero? || altura.to_f.zero?
 				true
-			end
+			end # fim do método validar_dimensoes
 
-		end
+		end # fim da classe ImpImg
 	end
 end
